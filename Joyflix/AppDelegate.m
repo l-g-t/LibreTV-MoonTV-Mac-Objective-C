aa2cbb6c3997b766613ae394993356018f218f08
jeffernn <624670457@qq.com>
2026-05-13 21:42:55 +0800

    ～～～

--- Joyflix/AppDelegate.m
+++ Joyflix/AppDelegate.m
@@ -98,7 +98,7 @@
 }
 
 - (void)checkForUpdatesWithURL:(NSString *)urlString retryLevel:(NSInteger)retryLevel isManualCheck:(BOOL)isManualCheck {
-    NSString *currentVersion = @"1.5.2";
+    NSString *currentVersion = @"1.5.3";
     NSURL *url = [NSURL URLWithString:urlString];
 
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
